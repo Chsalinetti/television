8b067d7652e937394a08750693bc79fc22e19e1e
Chsalinetti <cws2525@rit.edu>
2022-03-01 16:56:50 -0500

    created secrets.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import evan from './evan.jpg';
 
+const REDIRECT_URI = "http://localhost:3000"
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+const RESPONSE_TYPE = "token"
+
 function App() {
   return (
     <div className="App">
