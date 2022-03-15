ac2ad4219a10da67dbe473e02ba461639876baf8
Chsalinetti <cws2525@rit.edu>
2022-03-15 19:54:52 -0400

    change

--- my-app/src/Image.js
+++ my-app/src/Image.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './Image.css';
 import radiohead from './images/radiohead.gif';
-import psy1 from './images/psy1.gif';
+import im from './images/psy2.gif';
 
 class Image extends Component {
 
     render() {
         return (
             <div>
-                    <img className="photo" src={psy1}/>
+                    <img className="photo" src={im}/>
             </div>
         )
     }
