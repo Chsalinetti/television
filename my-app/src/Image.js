9d21bd0709ec487f50a6556154df8bb0aaedf310
Chsalinetti <cws2525@rit.edu>
2022-03-15 20:51:43 -0400

    gif

--- my-app/src/Image.js
+++ my-app/src/Image.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './Image.css';
-import im from './images/stars2.gif';
+import im from './images/fire1.gif';
 
 class Image extends Component {
 
