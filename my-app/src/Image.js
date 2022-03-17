df53524be0040de33e17408caedb0aa0aca46118
Chsalinetti <cws2525@rit.edu>
2022-03-17 00:59:53 -0400

    image update

--- my-app/src/Image.js
+++ my-app/src/Image.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './Image.css';
-import im from './images/quinn.gif';
+import im from './images/dusk.webp';
 
 class Image extends Component {
 
