e5f5e9b0b1ce8daaad2940f7eb1a6e42c0e829c3
Chsalinetti <cws2525@rit.edu>
2022-03-26 00:09:45 -0400

    album art displayed correctly

--- my-app/src/AlbumArt.js
+++ my-app/src/AlbumArt.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './AlbumArt.css';
 
+//https://i.scdn.co/image/ab67616d0000b273ee6c91661e0c19cd9514d0da
+
 const REDIRECT_URI = "http://localhost:3000"
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
 const CLIENT_ID = "ddc52ef734194f2492bc8bc09c0fe151"
@@ -10,12 +12,10 @@ const CLIENT_ID = "ddc52ef734194f2492bc8bc09c0fe151"
 class AlbumArt extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {track: "https://i.scdn.co/image/ab67616d0000b273ee6c91661e0c19cd9514d0da"};
   }
 
 renderArt=()=> {
-  return (<img className="photo" src={this.state.track}/>);
+  return (<img className="photo" src={this.props.albumArt}/>);
 }
 
   render() {
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,36 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 import AlbumArt from './AlbumArt';
 import Image from './Image';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Image/>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {albumArt: 'https://i.scdn.co/image/ab67616d0000b273ee6c91661e0c19cd9514d0da'};
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => this.setState({ time: Date.now() }), 5000);
+  }
+  /**
+   * Resets image update timer
+   */
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
+
+  renderApp=()=> {
+    return (<AlbumArt albumArt={this.state.albumArt}/>);
+  }
+  render() {
+    return(
+      <div className="App">
+      {this.renderApp()}
+      </div>
+    )
+  }
+}
 export default App;
+
